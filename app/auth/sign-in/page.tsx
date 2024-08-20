74d4629b42f4037a39b74bfc222d0cd4247611fb
Victor Guedes <victorfelipe.guedes@hotmail.com>
2024-08-20 20:07:04 -0300

    feat: create sign-in/sign-up initial ui

---
+++ app/auth/sign-in/page.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+
+import { Button } from "~/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+
+export const metadata = {
+  title: "Entrar na conta",
+};
+
+export default function SignIn() {
+  return (
+    <Card className="mx-auto w-[420px]">
+      <CardHeader>
+        <CardTitle className="text-2xl">Login</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">E-mail</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="max@email.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">Senha</Label>
+              <Link href="#" className="ml-auto inline-block text-sm underline">
+                Esqueceu a sua senha?
+              </Link>
+            </div>
+            <Input id="password" type="password" required />
+          </div>
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+        </div>
+        <div className="mt-4 text-center text-sm">
+          Não tem uma conta?{" "}
+          <Link href="/auth/sign-up" className="underline">
+            Criar
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
