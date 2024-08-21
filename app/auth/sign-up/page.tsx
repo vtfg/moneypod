acc457ceceb7ef23f10e4f8a77466d24bbc9ffd6
Victor Guedes <victorfelipe.guedes@hotmail.com>
2024-08-20 21:49:48 -0300

    refactor: move sign-up form to a separate file

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,66 +1,9 @@
-import Link from "next/link";
-
-import { Button } from "~/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
-import { Input } from "~/components/ui/input";
-import { Label } from "~/components/ui/label";
+import SignUpForm from "~/components/forms/auth/sign-up-form";
 
 export const metadata = {
   title: "Crie sua conta",
 };
 
 export default function SignUp() {
-  return (
-    <Card className="mx-auto w-[420px]">
-      <CardHeader>
-        <CardTitle className="text-xl">Cadastre-se</CardTitle>
-        <CardDescription>
-          e tenha acesso a uma plataforma completa para controle financeiro,
-          pessoal e profissional.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="grid gap-4">
-          <div className="grid grid-cols-2 gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="first-name">Nome</Label>
-              <Input id="first-name" placeholder="Max" required />
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="last-name">Sobrenome</Label>
-              <Input id="last-name" placeholder="Robinson" required />
-            </div>
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="email">E-mail</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="max@email.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Senha</Label>
-            <Input id="password" type="password" />
-          </div>
-          <Button type="submit" className="w-full">
-            Criar minha conta
-          </Button>
-        </div>
-        <div className="mt-4 text-center text-sm">
-          Já tem uma conta?{" "}
-          <Link href="/auth/sign-in" className="underline">
-            Entrar
-          </Link>
-        </div>
-      </CardContent>
-    </Card>
-  );
+  return <SignUpForm />;
 }
