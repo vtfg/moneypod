6e963281d993cba2e322beb8a0ae0de2b60b55cc
Victor Guedes <victorfelipe.guedes@hotmail.com>
2024-08-20 22:08:35 -0300

    feat: add sign-up form validation with react-hook-form
    - add form shadcn/ui components
    - create passwordSchema util

--- components/forms/auth/sign-up-form.tsx
+++ components/forms/auth/sign-up-form.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { FormEvent } from "react";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 
 import { Button } from "~/components/ui/button";
 import {
@@ -11,16 +13,47 @@ import {
   CardHeader,
   CardTitle,
 } from "~/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
 import { Input } from "~/components/ui/input";
-import { Label } from "~/components/ui/label";
+import { passwordSchema } from "~/lib/utils";
 
 export const metadata = {
   title: "Crie sua conta",
 };
 
+const schema = z.object({
+  first_name: z
+    .string({ required_error: "Nome é um campo obrigatório" })
+    .min(2, {
+      message: "Nome deve ter no mínimo 2 caracteres.",
+    }),
+  last_name: z
+    .string({ required_error: "Sobrenome é um campo obrigatório" })
+    .min(2, {
+      message: "Sobrenome deve ter no mínimo 2 caracteres.",
+    }),
+  email: z
+    .string({ required_error: "E-mail é um campo obrigatório" })
+    .email({ message: "E-mail deve ser válido" }),
+  password: passwordSchema,
+});
+
+type FormData = z.infer<typeof schema>;
+
 export default function SignUpForm() {
-  async function handleSubmit(event: FormEvent) {
-    event.preventDefault();
+  const form = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  async function handleSignUp(data: FormData) {
+    console.log(data);
   }
 
   return (
@@ -33,42 +66,84 @@ export default function SignUpForm() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <form onSubmit={handleSubmit}>
-          <div className="grid gap-4">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSignUp)}
+            className="grid gap-4"
+          >
             <div className="grid grid-cols-2 gap-4">
-              <div className="grid gap-2">
-                <Label htmlFor="first-name">Nome</Label>
-                <Input id="first-name" placeholder="Max" required />
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="last-name">Sobrenome</Label>
-                <Input id="last-name" placeholder="Robinson" required />
-              </div>
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="email">E-mail</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="max@email.com"
-                required
+              <FormField
+                control={form.control}
+                name="first_name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nome</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Max" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="last_name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Sobrenome</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Robinson" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
             </div>
-            <div className="grid gap-2">
-              <Label htmlFor="password">Senha</Label>
-              <Input id="password" type="password" />
-            </div>
-            <Button type="submit" className="w-full" onClick={handleSubmit}>
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>E-mail</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="max@robinson.com"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Senha</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full">
               Criar minha conta
             </Button>
-          </div>
-          <div className="mt-4 text-center text-sm">
-            Já tem uma conta?{" "}
-            <Link href="/auth/sign-in" className="underline">
-              Entrar
-            </Link>
-          </div>
-        </form>
+
+            <div className="mt-4 text-center text-sm">
+              Já tem uma conta?{" "}
+              <Link href="/auth/sign-in" className="underline">
+                Entrar
+              </Link>
+            </div>
+          </form>
+        </Form>
       </CardContent>
     </Card>
   );
