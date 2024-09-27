2928c6b882d2fdcb0e0c21f1d598613d11a49b2b
Victor Guedes <victorfelipe.guedes@hotmail.com>
2024-09-27 15:33:56 -0300

    feat: generate supabase typings

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,10 +1,12 @@
 import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
 
+import { Database } from "~/lib/types/supabase";
+
 export function createClient() {
   const cookieStore = cookies();
 
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
