85f4a9ebab861f9aebe53ae07b5dd4334f1719c3
Victor Guedes <victorfelipe.guedes@hotmail.com>
2024-10-02 11:32:16 -0300

    feat: add links to sidebar

--- components/dashboard/sidebar.tsx
+++ components/dashboard/sidebar.tsx
@@ -1,39 +1,66 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { Cog, Coins, LineChart, Menu, Table } from "lucide-react";
 
 import { Button } from "~/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "~/components/ui/sheet";
 import { Separator } from "~/components/ui/separator";
 import { Logo } from "~/components/logo";
+import { cn } from "~/lib/utils";
 
 export function DesktopSidebar() {
+  const currentPath = usePathname();
+
   return (
     <div className="flex-1">
       <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
         <Link
-          href="#"
-          className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
+          href="/dashboard"
+          className={cn(
+            "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary",
+            {
+              "bg-muted text-primary": currentPath === "/dashboard",
+            }
+          )}
         >
           <LineChart className="h-4 w-4" />
           Visão geral
         </Link>
         <Link
-          href="#"
-          className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+          href="/dashboard/budget"
+          className={cn(
+            "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary",
+            {
+              "bg-muted text-primary": currentPath === "/dashboard/budget",
+            }
+          )}
         >
           <Coins className="h-4 w-4" />
           Orçamento
         </Link>
         <Link
-          href="#"
-          className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+          href="/dashboard/transactions"
+          className={cn(
+            "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary",
+            {
+              "bg-muted text-primary":
+                currentPath === "/dashboard/transactions",
+            }
+          )}
         >
           <Table className="h-4 w-4" />
           Transações
         </Link>
         <Link
           href="#"
-          className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+          className={cn(
+            "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary",
+            {
+              "bg-muted text-primary": currentPath === "/dashboard/settings",
+            }
+          )}
         >
           <Cog className="h-4 w-4" />
           Configuração
